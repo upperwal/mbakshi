a53cf5a0e8e979de1494ec7664c1924256141a31
Abhishek Upperwal <a.upperwal@gmail.com>
2023-08-18 13:45:08 +0530

    move

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -20,41 +20,6 @@ export default function Projects() {
     return (
         <section className={styles.projects_container}>
             <div className={styles.heading}>
-                <h1><span className={styles.light}>ARCHITECTURE</span> PROJECTS</h1>
-            </div>
-            <Grid container spacing={12}>
-                <Grid item xs={12} md={6} lg={4}>
-                    <TextBox 
-                        title="CENTRE"
-                        category="THESIS"
-                        img={ThesisImg}
-                        description="Permaculture is a design system or a method that suggests a harmonious integration between human activities with natural environment, to provide for material and non- material human needs."
-                        link={'/documents/projects.pdf'}
-                    />
-                </Grid>
-                <Grid item xs={12} md={6} lg={4}>
-                    <TextBox 
-                        title="CHRONOS"
-                        category="RESIDENTIAL"
-                        img={ResidentialImg}
-                        description="The redevelopment of the residences revolves around taking architectural settings and breaking down the defined functional spaces to create a dynamic environment that serve a purpose."
-                        link={'/documents/projects.pdf'}
-                    />
-                </Grid>
-                <Grid item xs={12} md={6} lg={4}>
-                    <TextBox 
-                        title="BEYUL"
-                        category="LEISURE SPACE"
-                        img={LeisureImg}
-                        description="Within the buddhist doctrine, the percepts are meant to develop mind and character to make progress on the path of enlightenment, a state of highest happiness, bliss, fearlessness, freedom."
-                        link={'/documents/projects.pdf'}
-                    />
-                </Grid>
-            </Grid>
-
-            <div className={styles.space}> </div>
-
-            <div className={styles.heading}>
                 <h1><span className={styles.light}>PERSONAL</span> PROJECTS</h1>
             </div>
             <Grid container spacing={12}>
@@ -95,6 +60,48 @@ export default function Projects() {
                     />
                 </Grid> */}
             </Grid>
+
+
+            <div className={styles.space}> </div>
+
+
+
+            <div className={styles.heading}>
+                <h1><span className={styles.light}>ARCHITECTURE</span> PROJECTS</h1>
+            </div>
+            <Grid container spacing={12}>
+                <Grid item xs={12} md={6} lg={4}>
+                    <TextBox 
+                        title="CENTRE"
+                        category="THESIS"
+                        img={ThesisImg}
+                        description="Permaculture is a design system or a method that suggests a harmonious integration between human activities with natural environment, to provide for material and non- material human needs."
+                        link={'/documents/projects.pdf'}
+                    />
+                </Grid>
+                <Grid item xs={12} md={6} lg={4}>
+                    <TextBox 
+                        title="CHRONOS"
+                        category="RESIDENTIAL"
+                        img={ResidentialImg}
+                        description="The redevelopment of the residences revolves around taking architectural settings and breaking down the defined functional spaces to create a dynamic environment that serve a purpose."
+                        link={'/documents/projects.pdf'}
+                    />
+                </Grid>
+                <Grid item xs={12} md={6} lg={4}>
+                    <TextBox 
+                        title="BEYUL"
+                        category="LEISURE SPACE"
+                        img={LeisureImg}
+                        description="Within the buddhist doctrine, the percepts are meant to develop mind and character to make progress on the path of enlightenment, a state of highest happiness, bliss, fearlessness, freedom."
+                        link={'/documents/projects.pdf'}
+                    />
+                </Grid>
+            </Grid>
+
+            
+
+            
         </section>
     )
 }
